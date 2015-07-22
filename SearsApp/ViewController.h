e18211e80d5c604b2a456ae32184c510401ff2a2
Jung Kim <jungkim@Jungs-iMac.local>
2015-07-21 23:00:40 -0700

    Initial Commit

---
+++ SearsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearsApp
+//
+//  Created by Jung Kim on 7/21/15.
+//  Copyright (c) 2015 Jung Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SearsApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SearsApp
+//
+//  Created by Jung Kim on 7/21/15.
+//  Copyright (c) 2015 Jung Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
